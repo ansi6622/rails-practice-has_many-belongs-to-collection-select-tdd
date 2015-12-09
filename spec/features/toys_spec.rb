7d12e5ba02f3a3953e1c225b37318523ead2e9e6
EmilyPlatzer <emilyplatzer@gmail.com>
2015-12-09 11:06:39 -0700

    Toy new page should have a collection select for all Cats
    Upgraaaaad our object creation methods

--- spec/features/toys_spec.rb
+++ spec/features/toys_spec.rb
@@ -14,4 +14,16 @@ feature 'Toys' do
     visit toy_path(toy)
     expect(page).to have_content(cat.name)
   end
+
+  scenario 'new should have a collection select for all Cats' do
+    cat1 = create_cat(name: "Awesomekitty")
+    cat2 = create_cat
+    cat3 = create_cat
+    cat4 = create_cat
+    visit new_toy_path
+    expect(page).to have_content("Awesomekitty")
+    expect(page).to have_content(cat2.name)
+    expect(page).to have_content(cat3.name)
+    expect(page).to have_content(cat4.name)
+  end
 end
--- spec/support/object_creation_methods.rb
+++ spec/support/object_creation_methods.rb
@@ -1,7 +1,12 @@
-def create_cat
-  Cat.create!(:name => 'Fluffy Boots')
+def create_cat(overrides = {})
+  Cat.create!({
+    :name => "Fluffy Boots #{rand(1000..9999)}"
+  }.merge(overrides))
 end
 
 def create_toy(cat)
-  Toy.create!(name: 'Lazer Pointer', cat_id: cat.id)
+  Toy.create!(
+    name: "Lazer Pointer #{rand(1000..9999)}",
+    cat_id: cat.id
+  )
 end
